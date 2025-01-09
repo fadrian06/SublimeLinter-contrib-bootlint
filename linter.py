5dc461be8b53878ec3c57bc9fe341e9461d98111
fadrian06 <franyeradriansanchez@gmail.com>
2025-01-09 01:09:21 -0400

    Add support for non line specified errors

--- linter.py
+++ linter.py
@@ -1,40 +1,51 @@
-#
-# linter.py
-# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
-#
-# Authors:
-# Thomas Lebeau
-# Patrick Ziegler
-#
-# Copyright (c) 2015
-#
-# License: MIT
-#
+import re
+from typing import Iterator
 
-"""This module exports the Bootlint plugin class."""
-
-from SublimeLinter.lint import PythonLinter, util
+from SublimeLinter.lint import NodeLinter
+from SublimeLinter.lint.linter import LintMatch
 
 
-class Bootlint(PythonLinter):
-
-    """Provides an interface to bootlint."""
+class Bootlint(NodeLinter):
     defaults = {
-        'selector': 'text.html.basic',
+        "selector": "text.html.basic"
     }
-    selector = 'text.html.basic'
-    cmd = 'bootlint ${temp_file}'
-    # version_args = '--version'
-    # version_re = r'(?P<version>\d+\.\d+\.\d+)'
-    # version_requirement = '>= 0.12.0'
-    regex = (
-        r'^.+?:'  # filename
-        r'(?P<line>\d+):(?P<col>\d+) '
-        r'(?:(?P<error>[E])|(?P<warning>[W]))\d+ '
-        r'(?P<message>.+)'
-    )
-    multiline = True
-    line_col_base = (1, 1)
-    tempfile_suffix = 'html'
-    error_stream = util.STREAM_BOTH
-    check_version = True
+
+    cmd = "bootlint $file_name"
+
+    def find_errors(self, output: str) -> Iterator[LintMatch]:
+        lintMatches = []
+        cleanedOutput = output.replace("<", "&lt;").replace(">", "&gt;")
+
+        for line in cleanedOutput.splitlines():
+            match = re.match((
+                r"(?P<filename>\w+\.\w+):"
+                r"(?P<line>\d+)?:?"
+                r"(?P<col>\d+)?:?"
+                r" (?P<code>[A-Z\d]+)"
+                r" (?P<message>.+)"
+            ), line)
+
+            if match:
+                groupdict = match.groupdict()
+                code = groupdict.get("code")
+                errorType = "error" if not code or not code.startswith(
+                    "W") else "warning"
+
+                # print(groupdict)
+                # print("\n")
+                line = int(groupdict.get("line") or 1)
+                col = int(groupdict.get("col") or 1)
+
+                lintMatches.append(LintMatch({
+                    "filename": groupdict.get("filename"),
+                    "line": line - self.line_col_base[0],
+                    "col": col - self.line_col_base[1],
+                    "error_type": errorType,
+                    "code": groupdict.get("code"),
+                    "message": groupdict.get("message")
+                }))
+
+        # print(lintMatches)
+        # print("\n")
+
+        return iter(lintMatches)
